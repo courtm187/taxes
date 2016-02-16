65ff62cc63b79123bc75ab91208214578d4e68cf
Courtney Morrissey <courtneycmorrissey@gmail.com>
2016-02-15 18:57:19 -0700

    changed function to be 1 function with 4 parts

--- main.js
+++ main.js
@@ -1,16 +1,26 @@
-//question: how 
+//question: how
 
 module.exports = {
   calcTax: function(income){
-      return (income * 0.10);
+    if (income < 10){
+      var a;
+      a = income * 0.10;
+      return a;
+      }
+    if (income >= 10 && income < 20){
+      var b;
+      b = a + ((income - 10)*0.07);
+      return b;
     }
-  calcTaxB: function(income){
-      return (income * 0.07);
+    if (income >= 20 && income < 30){
+      var c;
+      c = b + ((income - 20)*0.05);
+      return c;
     }
-  calcTaxC: function(income){
-      return (income * 0.05);
-    }
-  calcTaxD: function(income){
-      return (income * 0.03);
+  if (income >=30){
+      var d;
+      d = c + ((income - 30)*0.03);
+      return d;
     }
   }
+};
--- test/test.js
+++ test/test.js
@@ -1,4 +1,11 @@
-
+// var code = require('../main.js');
+// var expect = require('chai').expect;
+//
+// describe('leapYear', function(){
+//   it('should say leap year', function(){
+//     expect(code.leapYear(2016)).to.equal("It is a leap year");
+//   });
+// });
 
 var code = require('../main.js');
 var expect = require('chai').expect;
@@ -8,23 +15,13 @@ describe('Tax Calculator', function(){
     expect(code.calcTax(1)).to.equal(0.1);
     expect(code.calcTax(10)).to.equal(1);
   });
-});
-
-
-describe('Tax Calculator', function(){
-  it('should tax 7% on the second $10', function(){
-    expect(code.calcTaxB(15)).to.equal(1.35);
-  });
-});
-
-describe('Tax Calculator', function(){
-  it('should tax 5% on the third $10', function(){
+  it('should tax 7% on the third $10', function(){
     expect(code.calcTaxC(15)).to.equal(1.35);
   });
-});
-
-describe('Tax Calculator', function(){
-  it('should tax 3% on the fourth $10', function(){
-    expect(code.calcTaxD(100)).to.equal(3);
+  it('should tax 5% on the third $10', function(){
+    expect(code.calcTaxC(25)).to.equal(1.85);
+  });
+  it('should tax 3% for the rest', function(){
+    expect(code.calcTaxC(35)).to.equal(2.15);
   });
 });
